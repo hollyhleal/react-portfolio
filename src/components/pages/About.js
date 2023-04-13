c7cfb032c4bfc7d35907db167588af101947d27f
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-12 18:59:02 -0600

    add template for About function

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex.
+      </p>
+    </div>
+  );
+}
