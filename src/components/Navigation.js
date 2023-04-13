30d2b6084e28d94f8b242dd70d6963131baffb24
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-12 22:49:55 -0600

    update imports and Navlinks

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,50 +1,50 @@
 // import "../styles/Navigation.css";
-import { Nav } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 
 function Navigation() {
   return (
     <Nav className="justify-content-end">
       <ul className="nav nav-tabs">
         <li className="nav-item">
-          <Nav.Link
+          <Link
             to="/react-portfolio"
             className={({ isActive, isPending }) =>
               isPending ? "pending" : isActive ? "active" : ""
             }
           >
             About Me
-          </Nav.Link>
+          </Link>
         </li>
         <li className="nav-item">
-          <Nav.Link
+          <Link
             to="/react-portfolio/portfolio"
             className={({ isActive, isPending }) =>
               isPending ? "pending" : isActive ? "active" : ""
             }
           >
             Portfolio
-          </Nav.Link>
+          </Link>
         </li>
         <li className="nav-item">
-          <Nav.Link
+          <Link
             to="/react-portfolio/contact"
             className={({ isActive, isPending }) =>
               isPending ? "pending" : isActive ? "active" : ""
             }
           >
             Contact
-          </Nav.Link>
+          </Link>
         </li>
         <li className="nav-item">
-          <Nav.Link
+          <Link
             to="/react-portfolio/resume"
             className={({ isActive, isPending }) =>
               isPending ? "pending" : isActive ? "active" : ""
             }
           >
             Resume
-          </Nav.Link>
+          </Link>
         </li>
       </ul>
     </Nav>
