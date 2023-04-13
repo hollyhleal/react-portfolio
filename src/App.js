a8fa3d2c68f01d69847c02207bb7944ea97dbc04
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-13 15:01:11 -0600

    update to where custom css is linked

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
 import Resume from "./components/pages/Resume";
-import "./App.css";
 
 function App() {
   return (
