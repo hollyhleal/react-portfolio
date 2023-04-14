a856d6141203f3f428458820203c0c7a00dad51e
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-14 15:26:03 -0600

    add emailjs and form fields

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,15 +1,49 @@
-import { Form, BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    let validEmail = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+
+    if (e.target.user_email.value.match(validEmail)) {
+      emailjs
+        .sendForm(
+          "service_4b80nrq",
+          "template_zm575ph",
+          form.current,
+          "kCWwtikSw6O5RMuPc"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      e.target.user_name.value = "";
+      e.target.user_email.value = "";
+      e.target.message.value = "";
+    } else {
+      alert("Please enter a valid email address in order to submit this form.");
+    }
+  };
+
   return (
     <div>
-      <h1>Questions or comments? Contact me here...</h1>
-      <Form method="post" action="/contact">
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <input type="text" name="message" />
-        <button type="submit">Contact Holly</button>
-      </Form>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
     </div>
   );
 }
