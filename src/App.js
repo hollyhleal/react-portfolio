3d22056b6fd226e5d5bcd657bcd2bd8cc8dc7114
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-13 18:39:57 -0600

    update placement of custom css file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
 import Resume from "./components/pages/Resume";
+import "./App.css";
 
 function App() {
   return (
