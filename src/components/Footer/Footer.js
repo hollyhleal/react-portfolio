21d6ccac8a61c0927b986e568524c7e7e3ae5798
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-14 13:40:05 -0600

    update styling for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="col">
+    <footer className="footer col p-3">
       <a
         href="https://github.com/hollyhleal"
         target="_blank"
@@ -52,7 +52,7 @@ function Footer() {
           <path d="M3.857 13.145h7.137v-1.428H3.857v1.428zM10.254 0 9.108.852l4.26 5.727 1.146-.852L10.254 0zm-3.54 3.377 5.484 4.567.913-1.097L7.627 2.28l-.914 1.097zM4.922 6.55l6.47 3.013.603-1.294-6.47-3.013-.603 1.294zm-.925 3.344 6.985 1.469.294-1.398-6.985-1.468-.294 1.397z" />
         </svg>
       </a>
-    </div>
+    </footer>
   );
 }
 
