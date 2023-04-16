adf74108e8269f3e312202aa1766b82ba3c66dc9
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-16 12:24:02 -0600

    adjust styling on project cards

--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Project from "../../Project";
+import "../Portfolio/Portfolio.css";
 
 function Portfolio() {
   return (
     <div className="container">
       <h2>Check out some of my latest work...</h2>
-        <Project />
+      <Project />
     </div>
   );
 }
