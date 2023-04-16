3180b29cf01045c68318bdbf2a27fada4a18e396
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-16 11:48:42 -0600

    clean up code

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -1,6 +1,4 @@
 import "../Resume/Resume.css";
-// import Card from "react-bootstrap/Card";
-// import CardGroup from "react-bootstrap/CardGroup";
 import Button from "react-bootstrap/Button";
 import resumePDF from "../../../documents/hl-react-portfolio-resume.pdf";
 
@@ -20,26 +18,6 @@ function Resume() {
           Download my Resume (PDF)
         </Button>
       </div>
-      {/* <CardGroup>
-        <Card>
-          <Card.Header as="h5">Front-End</Card.Header>
-          <Card.Body>
-            <Card.Text>My front-end skills include...</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Header as="h5">Back-End</Card.Header>
-          <Card.Body>
-            <Card.Text>My back-end skills include...</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Header as="h5">Non-Technical</Card.Header>
-          <Card.Body>
-            <Card.Text>Other skills include...</Card.Text>
-          </Card.Body>
-        </Card>
-      </CardGroup> */}
       <div class="card-deck">
         <div class="card">
           <div class="card-body">
