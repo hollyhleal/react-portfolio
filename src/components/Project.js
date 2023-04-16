3389c1f0b9870d3d95ddaf829a5c2a6e20a93801
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-16 12:26:10 -0600

    adjust header

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,29 +1,9 @@
-// import Card from "react-bootstrap/Card";
-// import Row from "react-bootstrap/Row";
-// import Button from "react-bootstrap/Button";
 import { projects } from "../ProjectData";
 
 function Project() {
   return (
     <div class="row row-cols-1 row-cols-md-3">
       {projects.map(({ image, title, description, deployed, repo }) => (
-        // <Card>
-        //   <Card.Img variant="top" src={image} />
-        //   <Card.Body>
-        //     <Card.Title>{title}</Card.Title>
-        //     <Card.Text>{description}</Card.Text>
-        //   </Card.Body>
-
-        //   <Card.Body>
-        //     <Button variant="primary" href={deployed} target="_blank">
-        //       Deployed Application
-        //     </Button>
-        //     {"   "}
-        //     <Button variant="secondary" href={repo} target="_blank">
-        //       Repository
-        //     </Button>
-        //   </Card.Body>
-        // </Card>
         <div class="col mb-4">
           <div class="card h-100">
             <img src={image} class="card-img-top" alt="..." />
--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -5,7 +5,7 @@ import "../Portfolio/Portfolio.css";
 function Portfolio() {
   return (
     <div className="container">
-      <h2>Check out some of my latest work...</h2>
+      <h3>Check out some of my latest work...</h3>
       <Project />
     </div>
   );
