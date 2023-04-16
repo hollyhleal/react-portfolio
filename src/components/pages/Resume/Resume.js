fadc53406644e198f30f4dbbb639d5132e680ed8
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-16 11:48:11 -0600

    update styling of cards

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -1,6 +1,6 @@
 import "../Resume/Resume.css";
-import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
+// import Card from "react-bootstrap/Card";
+// import CardGroup from "react-bootstrap/CardGroup";
 import Button from "react-bootstrap/Button";
 import resumePDF from "../../../documents/hl-react-portfolio-resume.pdf";
 
@@ -11,11 +11,16 @@ function Resume() {
         <h3>Resume & Skills</h3>
       </div>
       <div className="mb-2">
-        <Button bsPrefix="btn" size="lg" href={resumePDF} download="holly-leal-resume.pdf">
-            Download my Resume (PDF)
+        <Button
+          bsPrefix="btn"
+          size="lg"
+          href={resumePDF}
+          download="holly-leal-resume.pdf"
+        >
+          Download my Resume (PDF)
         </Button>
       </div>
-      <CardGroup>
+      {/* <CardGroup>
         <Card>
           <Card.Header as="h5">Front-End</Card.Header>
           <Card.Body>
@@ -34,7 +39,35 @@ function Resume() {
             <Card.Text>Other skills include...</Card.Text>
           </Card.Body>
         </Card>
-      </CardGroup>
+      </CardGroup> */}
+      <div class="card-deck">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Front-End Skills</h5>
+            <p class="card-text">
+              HTML5, CSS, JavaScript, JQuery, JSON, ES6, Bootstrap, AJAX
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Back-End Skills</h5>
+            <p class="card-text">
+              MySQL, MongoDB, Express, React, Node, Handlebars, Sequelize, NoSQL
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Full-Stack & Non-Technical Skills</h5>
+            <p class="card-text">
+              Agile, Kanban, Git Flow, Document Object Model (DOM), API
+              Integrations, Object-Oriented Programming, Testing, ProWorkFlow,
+              Jira, MERN Stack, Wireframes, Entity-Relationship Diagrams
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
